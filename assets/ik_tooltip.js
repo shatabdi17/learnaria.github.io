372611ce9f118263db41738911c2aa8b5f685b2f
Shatabdi Bhattacharjee <shatabdi17@gmail.com>
2018-10-06 17:24:42 -0400

    added accessible tooltip

--- assets/ik_tooltip.js
+++ assets/ik_tooltip.js
@@ -38,13 +38,19 @@
 				.addClass('ik_tooltip')
 				.attr({
 					'id': id,
+					'role': 'tooltip', // assign tooltip role
+					'aria-hidden': 'true', // hide it from screen reader to prevent it from been read twice
+					'aria-live': 'polite' // make it live region
 				});
 			
 			$elem
+				.attr({
+					'tabindex': 0 // add tab order
+				})
 				.css('position', 'relative')
 				.removeAttr('title') // remove title to prevent it from being read
 				.after($tooltip)
-				.on('mouseover', function(event) {
+				.on('mouseover focus', function(event) {
 					
 					var y, x;
 					
@@ -62,7 +68,10 @@
 					}
 					
 					$tooltip // position and show tooltip
-						.css({
+					.attr({
+						'aria-hidden': 'false'
+						})
+					.css({
 							'top': y, 
 							'left': x
 						})
@@ -73,11 +82,33 @@
 					if (!$(event.currentTarget).is(':focus') ) { // hide tooltip if current element is not focused
 						
 						$tooltip
+						.attr({
+							'aria-hidden': 'true'
+							})
 							.removeClass('visible mouseover');					
 					}
 										
 				})
+				.on('blur', function(event) {              
+					if (!$tooltip.hasClass('mouseover') ) { // hide tooltip if mouse is not over the current element               
+					$tooltip
+						.attr({
+							'aria-hidden': 'true'
+						})
+						.removeClass('visible');       
+					}
+				})
+				.on('keyup', function(event) {         
+					if(event.keyCode == ik_utils.keys.esc) { // hide when escape key is pressed
+						$tooltip
+							.attr({
+								'aria-hidden': 'true'
+							})
+							.removeClass('visible');
+					}              
+				});
 		}
+		
 	};
 	
 	$.fn[pluginName] = function ( options ) {
