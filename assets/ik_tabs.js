a61edc1e61ccffff01b4bec956a8a6ef98941a2a
Shatabdi Bhattacharjee <shatabdi17@gmail.com>
2018-10-14 19:38:08 -0400

    added accessible tabs

--- assets/ik_tabs.js
+++ assets/ik_tabs.js
@@ -34,6 +34,9 @@
 		
 		$tabbar = $('<ul/>') // create ul element to hold all tabs
 			.addClass('ik_tabbar cf')
+			.attr({
+				'role': 'tablist' // add tablistr role
+			})
 			.prependTo($elem);
 		
 		plugin.panels = $elem // initialize panels and create tabs
@@ -43,7 +46,10 @@
 				var $tab, $panel, lbl;
 				
 				$panel = $(el).attr({
-					'id': id + '_panel' + i  // add unique id for a panel					
+					 'id': id + '_panel' + i,  // add unique id for a panel	
+					 'role': 'tabpanel', // add tabpanel role
+					 'aria-hidden': true, // initially hide from screen readers
+					 'tabindex': 0 // add to tab order						 
 				})
 				.addClass('ik_tabpanel')
 				.hide();
@@ -53,10 +59,12 @@
 				$panel.removeAttr('title');
 				
 				$tab = $('<li/>').attr({
-					'id': id + '_tab' + i // create unique id for a tab
-
+					'id': id + '_tab' + i, // create unique id for a tab
+					'role': 'tab', // assign tab role
+    				'aria-controls': 'panel' + i // define which panel it controls
 				})
 				.text(lbl > '' ? lbl : 'Tab ' + (i + 1))
+				 .on('keydown', {'plugin': plugin, 'index': i}, plugin.onKeyDown) // add keyboard event handler
 				.on('click', {'plugin': plugin, 'index': i}, plugin.selectTab) // add mouse event handler
 				.appendTo($tabbar);
 			});
@@ -70,7 +78,37 @@
 			}
 		});
 	};
-	
+	/**
+	* Handles keydown event on header button.
+	*/
+	Plugin.prototype.onKeyDown = function (event) {
+		var plugin = event.data.plugin,
+				ind = event.data.index,
+				$tabs,
+				$panels,
+				next;
+					
+		$elem = plugin.element;
+		$tabs = plugin.tabs;
+		$panels = plugin.panels;
+			
+		switch (event.keyCode) {
+				case ik_utils.keys.left:
+				case ik_utils.keys.up:
+						next = ind > 0 ? --ind : 0;
+						plugin.selectTab({data:{'plugin': plugin, 'index': next}});
+						break;
+				case ik_utils.keys.right:
+				case ik_utils.keys.down:
+						next = ind < $tabs.length - 1 ? ++ind : $tabs.length - 1;
+						plugin.selectTab({data:{'plugin': plugin, 'index': next}});
+						break;
+				case ik_utils.keys.space:
+						event.preventDefault();
+						event.stopPropagation();
+						return false;
+		}
+	}
 	/** 
 	 * Selects specified tab.
 	 * 
@@ -92,18 +130,32 @@
 		
 		$tabs // deselect all tabs
 			.removeClass('selected')
+			.attr({
+				'aria-selected': false,
+				'tabindex': -1 // remove them from tab order
+			})
 			.blur();
 		
 		$($tabs[ind]) // select specified tab
-			.addClass('selected');
+			.addClass('selected')
+			.attr({
+				'aria-selected': true,
+				tabindex: 0
+			});
 		
 		if (event.type) $($tabs[ind]).focus(); // move focus to current tab if reached by mouse or keyboard
 		
 		$panels // hide all panels
-			.hide(); 
+		.attr({
+			'aria-hidden': true
+		})
+		.hide(); 
 		
 		$($panels[ind]) // show current panel
-			.show(); 
+		.attr({
+			'aria-hidden': false
+		})
+		.show(); 
 		
 	}
 	
